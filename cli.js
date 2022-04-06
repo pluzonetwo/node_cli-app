b227c68f3d49f71f4bb1567e5bca6d1082e53192
Konstantin Os <permkonstantin@gmail.com>
2022-04-06 22:06:35 +0500

    Lesson_4_practice_ready

--- cli.js
+++ cli.js
@@ -1,34 +1,92 @@
 #!/home/pluzonetwo/.nvm/versions/node/v16.14.2/bin/node
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs/promises');
+const { lstatSync } = require('fs');
 const inquirer = require('inquirer');
+const yargs = require('yargs');
+const path = require('path');
 
-const currentDirectory = process.cwd();
+let currentDirectory = process.cwd();
 
-const isFile = fileName => { // вернет true, если входящий аргумент - файл, false, если папка
-    return fs.lstatSync(fileName).isFile(); // сюда передается имя, которое нужно проверить на соответствие файлу или папке
+const options = yargs
+    .positional('d', {
+        describe: 'Path to directory',
+        default: process.cwd(),
+    })
+    .positional('p', {
+        describe: 'Pattern',
+        default: '',
+    }).argv;
+console.log(options);
+
+class ListItem {
+    constructor(path, fileName) {
+        this.path = path;
+        this.fileName = fileName;
+    }
+
+    get isDir() {
+        return lstatSync(this.path).isDirectory();
+    }
 }
 
-const list = fs.readdirSync(currentDirectory).filter(isFile); // сюда записываем список нужных файлов и папок
+const run = async () => {
+    const list = await fs.readdir(currentDirectory);
+    const items = list.map(fileName =>
+        new ListItem(path.join(currentDirectory, fileName), fileName));
+
+    const item = await inquirer
+        .prompt([
+            {
+                name: 'fileName',
+                type: 'list',
+                message: `Choose: ${currentDirectory}`,
+                choices: items.map(item => ({name: item.fileName, value: item})),
+            }
+        ])
+        .then(answer => answer.fileName);
+
+    if (item.isDir) {
+        currentDirectory = item.path;
+        return await run();
+    } else {
+        const data = await fs.readFile(item.path, 'utf-8');
+
+        if (options.p == null) console.log(data);
+        else {
+            const regExp = new RegExp(options.p, 'utf');
+            console.log(data.match(regExp));
+        }
+    }
+};
+
+run();
+
+
+
+// const isFile = (fileName) => fs.lstatSync(fileName).isFile(); // метод возвращает true, если текущий файл - файл
+
+// const fileList = fs.readdirSync(currentDirectory);
+    // .filter(isFile); // сюда записываем список нужных файлов и папок
 
 //создаем меню
-inquirer // промис
-    .prompt([{
-        name: 'fileName', // название переменной, по которой будет доступно введенное пользователем значение
-        type: 'list', // тип вопроса (список в методичке)
-        message: 'Choose file:', // сообщение для пользователя
-        choices: list, // варианты ответа для пользователя
-    }])
-    .then((answer) => {
-        console.log(answer.fileName);
-        const filePath = path.join(currentDirectory, answer.fileName);
-
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-            console.log(data);
-        });
-    });
-
+// inquirer // промис
+//     .prompt([
+//         {
+//             name: 'dirName', // название переменной, по которой будет доступно введенное пользователем значение
+//             type: 'input', // тип вопроса (список в методичке)
+//             message: 'Enter dir name:', // сообщение для пользователя
+//             // choices: fileList, // варианты ответа для пользователя
+//         }
+//     ])
+//     .then(({ dirName }) => {
+//         console.log(dirName);
+//         // const fullPath = path.join(__dirname, dirName);
+//         // const data = fs.readdirSync(fullPath, 'utf-8');
+//         //
+//         // console.log(data);
+//     })
+//
 // const rl = readLine.createInterface({ // создаем интерфейс ридлайна
 //     input: process.stdin, // потоки консоли, входящие/исходящие
 //     output: process.stdout,
